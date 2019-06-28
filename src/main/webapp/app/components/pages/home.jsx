4f84b95ba49c2659ec5b877865531db5b37ed7d1
nycrera91 <nycrera@gmail.com>
2019-06-28 23:47:06 +0300

    Home header eklendi.

--- src/main/webapp/app/components/pages/home.jsx
+++ src/main/webapp/app/components/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Questions from '../items/questions';
+import {t} from "localizify";
 
 var createReactClass = require('create-react-class');
 var HomePage = createReactClass({
   render() {
     return (
       <div>
-        <Questions fetched="false" />
+          <h1>{t('projectName')}</h1>
+          <Questions fetched="false" />
       </div>
     );
   }
